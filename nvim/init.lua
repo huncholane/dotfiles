cca6ed96c998f5fbc62c044c13db90e9fa436148
huncholane <huncho@hygo.ai>
2025-06-19 15:27:26 -0700

    correctly identify readonly buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,13 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-if vim.o.readonly then
+local function has_flag(flag)
+  for _, arg in ipairs(vim.v.argv) do
+    if arg == flag then
+      return true
+    end
+  end
+  return false
+end
+
+if has_flag("-R") then
   require("config.vr")
 else
   require("config.lazy")
