18209eeec7afd6e1e539c5f6794a5a0b191bd20a
huncholane <huncho@hygo.ai>
2025-09-22 18:34:38 -0700

    feat(nvim): pimped out the lua lsp

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,3 +1,4 @@
+local library = vim.api.nvim_get_runtime_file("", true)
 return {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
@@ -6,11 +7,18 @@ return {
 		Lua = {
 			runtime = {
 				version = "LuaJIT",
+				-- make sure the full path starts with lua/
+				-- origin example: choose plugins/lua/cmp/init.lua over plugins/cargo/completions/cmp.lua
+				path = {
+					"lua/?/init.lua",
+					"lua/?.lua",
+				},
 			},
-			diagnostics = {
-				globals = {"vim"},
+			workspace = {
+				library = library,
+				checkThirdParty = false,
 			},
-			signatureHelp = { enabled = true },
+			telemetry = { enable = false },
 		},
 	},
 }
