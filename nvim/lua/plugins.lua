f6e79c832d2df15abd5016f5c2df7b55f0f5e5d4
huncholane <huncho@hygo.ai>
2025-10-13 12:02:44 -0700

    feat(nvim): add npm compiler

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,7 +21,7 @@ local plugins = {
 -- Load plugins from lua/plugins
 for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/plugins")) do
   if file:sub(-4) == ".lua" then
-    local mod_str = 'plugins.'..file:sub(1, -5)
+    local mod_str = 'plugins.' .. file:sub(1, -5)
     local ok, mod = pcall(require, mod_str)
     if ok then
       table.insert(plugins, mod)
